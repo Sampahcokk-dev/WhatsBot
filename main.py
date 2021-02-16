e2a25532fb7e573cf987e7eb7d9c7acea283792a
Sampahcokk-dev <yudaplzhacker@gmail.com>
2021-02-17 06:52:01 +0800

    4.1

--- main.py
+++ main.py
@@ -424,7 +424,7 @@ def main():
 			jadi=web[i].findChildren()
 			surah.append("ayat"+str(i)+":"+jadi[0].text+"\nbacaan :"+jadi[1].text+"\narti :"+jadi[2].text)
 
-		surahJadi=''.join(strd)
+		surahJadi=''.join(surah)
 		reply={"reply":surahJadi}
 		
 
