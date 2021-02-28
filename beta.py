10744d7297af77bb28865f2490e6404712b46bd6
Sampahcokk-dev <yudaplzhacker@gmail.com>
2021-02-28 10:09:09 +0800

    jadi

--- beta.py
+++ beta.py
@@ -5,7 +5,8 @@ import re
 param = {"q": "smp ypvdp"} 
 
 a=[]
-
+b=[]
+c=[]
 headers = {
     "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15"
@@ -19,17 +20,17 @@ soup.prettify()
 title = soup.select(".DKV0Md span")
 
 for t in title:
-    a.append(f"Title: {t.get_text()}\n")
+    a.append(f"{t.get_text()}\n")
 
 snippets = soup.select(".aCOpRe span:not(.f)")
 
 for d in snippets:
-    print(f"Snippet: {d.get_text()}\n")
+    b.append(f"{d.get_text()}\n\n")
 
-link = soup.findAll("a")
 
-for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-    print(re.split(":(?=http)", link["href"].replace("/url?q=", "")))
+print("="*20)
 
-print("="*20,"\n\n",a[9])
+for i in range(5):
+	c.append(a[i]+b[i])
 
+print(c[0]+c[1])
