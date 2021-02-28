78bc926d3ff0711237a2468ae77c014c2dec24b3
Sampahcokk-dev <yudaplzhacker@gmail.com>
2021-02-28 09:00:24 +0800

    Signed-off-by: Sampahcokk-dev <yudaplzhacker@gmail.com>

--- beta.py
+++ beta.py
@@ -28,4 +28,4 @@ link = soup.findAll("a")
 for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
     print(re.split(":(?=http)", link["href"].replace("/url?q=", "")))
 
-print(a)
+print("="*20,a)
